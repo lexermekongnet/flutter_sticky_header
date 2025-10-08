99590fc04e719339906ac0b3f4d9610171d437a0
Lex <lexer.sodela@corp.mekongnet.com.kh>
2025-10-08 10:57:45 +0700

    Remove debug print statement from hitTest in RenderSliverStickyHeader

--- lib/src/rendering/sliver_sticky_header.dart
+++ lib/src/rendering/sliver_sticky_header.dart
@@ -360,7 +360,6 @@ class RenderSliverStickyHeader extends RenderSliver with RenderSliverHelpers {
                   mainAxisPosition: mainAxisPosition,
                   crossAxisPosition: crossAxisPosition));
     } else if (child != null && child!.geometry!.hitTestExtent > 0.0) {
-      print('testing child');
       return child!.hitTest(result,
           mainAxisPosition: mainAxisPosition,
           crossAxisPosition: crossAxisPosition);
